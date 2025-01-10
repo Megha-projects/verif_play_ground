bc9fa752b8a6c7f0429a2cdadbc8875fd6851382
megha <meghasinghnamo@gmail.com>
2025-01-10 14:17:31 +0530

    changed in setting

--- verif_play_ground/settings.py
+++ verif_play_ground/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*1vqznd*-a%n^((zt!fsu5-#^73mdi9%_7+gc_me@(@k-!mn_9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
